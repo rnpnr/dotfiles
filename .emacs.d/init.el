fb3e36fdc35ae7400b58fa0d163dcb9f7657b843
0x766F6964 <palamar@ualberta.ca>
2019-09-28 10:01:22 -0600

    update .emacs.d/config.org
    added: which-key package
           code to make buffers more usable

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit org-bullets use-package))))
+ '(package-selected-packages (quote (which-key magit org-bullets use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
