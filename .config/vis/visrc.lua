650957fd2f324a5f7fbeb42931eb6e90dbd647ab
Randy Palamar <palamar@ualberta.ca>
2021-03-02 17:24:01 -0700

    add gq shortcut for formatting a paragraph and enable relative line numbers

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -3,4 +3,10 @@ require('vis')
 vis.events.subscribe(vis.events.INIT, function()
 	vis:command("set theme dark")
 	vis:command("set ai")
+
+	vis:command("map normal gq vip=<Escape>")
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	vis:command("set rnu")
 end)
