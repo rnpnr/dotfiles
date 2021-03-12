3d65faf28bd404f81bec68a036cd6ae058475ba3
Randy Palamar <palamar@ualberta.ca>
2021-03-11 23:08:52 -0700

    add cron script for updating RSS feeds with sfeed

---
+++ .config/cron/rssup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# updates rss feeds with sfeed
+# sends a notification when a new entry is found
+
+[ ${#} -lt 1 ] && (
+	echo 'Wrong number of arguments'
+	exit 1
+)
+
+configdir=~/.config/sfeed
+
+tmp=$(mktemp)
+trap '[ -f "$tmp" ] && rm $tmp' EXIT HUP INT
+
+update() {
+	sfeed_update $configdir/${1}.rss
+	wait
+	html ${1}
+}
+
+html() {
+	sfeed_html $sfeedpath/* >| $tmp
+	compare ${1}
+}
+
+compare() {
+	cmp -s $tmp $configdir/html/${1}.html || (
+		mv $tmp $configdir/html/${1}.html &&
+		notify-send "New RSS Entries: ${1}"
+	)
+}
+
+for arg; do
+	. $configdir/${arg}.rss
+	[ -d "$sfeedpath" ] || mkdir -p "$sfeedpath"
+	update $arg
+done
