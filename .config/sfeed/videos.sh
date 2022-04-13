c1200219867b359d22e5a2db8ef17dccecc3a01c
Randy Palamar <palamar@ualberta.ca>
2022-04-13 09:09:44 -0600

    sfeed: route lbry through odysee

--- .config/sfeed/videos.sh
+++ .config/sfeed/videos.sh
@@ -4,6 +4,11 @@ feeds() {
 	. ~/.config/sfeed/feeds/videos
 }
 
+filter() {
+	# replace lbry links with odysee links
+	sed '/lbry:\/\//s;lbry://;https://odysee.com/;'
+}
+
 order() {
 	# only keep newest 6 entries
 	sort -t '	' -k1rn,1 | sed 6q
