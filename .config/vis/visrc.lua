903562672020002a0d09532bbf12226b49573047
Randy Palamar <palamar@ualberta.ca>
2022-11-23 08:55:59 -0700

    vis:feedkeys -> vis:command this is more efficient

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -14,7 +14,7 @@ vis.events.subscribe(vis.events.INIT, function()
 	vis:command("map visual ,s :|sort<Enter>")
 
 	vis:map(vis.modes.NORMAL, "vo", function()
-			vis:feedkeys(":x/[ \t\r]+$/ d<Enter>")
+			vis:command("x/[ \t\r]+$/ d")
 		end, "remove spaces, tabs, and \r from end of all lines")
 end)
 
