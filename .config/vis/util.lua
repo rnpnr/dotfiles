146caf0baaee4aca5712b323698079082fed5670
Randy Palamar <palamar@ualberta.ca>
2023-01-05 07:47:39 -0700

    vis: util: find last occurence of '.' when splitting ext
    also use string methods rather than string.fn()

--- .config/vis/util.lua
+++ .config/vis/util.lua
@@ -1,7 +1,7 @@
 util = {}
 function util:splitext(file)
 	if file == nil then return nil, nil end
-	local i = string.find(file, '%.')
+	local i = file:reverse():find('%.')
 	if i == nil then return file, nil end
-	return string.sub(file, 0, i - 1), string.sub(file, i)
+	return file:sub(0, -(i + 1)), file:sub(-i)
 end
