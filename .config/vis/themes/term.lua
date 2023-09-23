eed9ccd304fcfaacf9ff87e9061830cf4b6f872b
Randy Palamar <palamar@ualberta.ca>
2023-09-22 22:10:35 -0600

    vis: term.lua: add some styles from updated lexers

--- .config/vis/themes/term.lua
+++ .config/vis/themes/term.lua
@@ -4,12 +4,16 @@ local grey0 = '#555551'
 
 lexers.STYLE_DEFAULT = ''
 lexers.STYLE_NOTHING = ''
-lexers.STYLE_CLASS = ''
+lexers.STYLE_ATTRIBUTE = 'fore:,bold'
+lexers.STYLE_CLASS = 'fore:,bold'
 lexers.STYLE_COMMENT = ''
 lexers.STYLE_CONSTANT = ''
 lexers.STYLE_DEFINITION = ''
 lexers.STYLE_ERROR = 'fore:red'
 lexers.STYLE_FUNCTION = ''
+lexers.STYLE_FUNCTION_BUILTIN = lexers.STYLE_FUNCTION
+lexers.STYLE_FUNCTION_METHOD = lexers.STYLE_FUNCTION
+lexers.STYLE_HEADING = 'fore:,bold'
 lexers.STYLE_KEYWORD = 'fore:,bold'
 lexers.STYLE_LABEL = 'fore:,bold'
 lexers.STYLE_NUMBER = ''
@@ -36,3 +40,14 @@ lexers.STYLE_STATUS_FOCUSED = lexers.STYLE_STATUS .. ',bold'
 lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
 lexers.STYLE_INFO = 'fore:,back:,bold'
 lexers.STYLE_EOF = ''
+
+-- diff
+lexers.STYLE_ADDITION = 'fore:green'
+lexers.STYLE_CHANGE = 'fore:yellow'
+lexers.STYLE_DELETION = 'fore:red'
+
+-- latex, tex, texinfo
+lexers.STYLE_COMMAND = lexers.STYLE_KEYWORD
+lexers.STYLE_COMMAND_SECTION = lexers.STYLE_CLASS
+lexers.STYLE_ENVIRONMENT = lexers.STYLE_TYPE
+lexers.STYLE_ENVIRONMENT_MATH = lexers.STYLE_NUMBER
